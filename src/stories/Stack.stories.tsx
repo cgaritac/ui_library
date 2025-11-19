78d0c0550f5bea4eb834b958b7408707900a305a
carlos.garita <carlos.garita@factork-software.com>
2025-11-19 02:07:36 -0600

    Fix: Error

--- src/stories/Stack.stories.tsx
+++ src/stories/Stack.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import type { ComponentProps } from "react";
-import { Stack } from "../components/stack/stack";
+import { Stack } from "../components/stack/Stack";
 import './stories.css';
 
 type StoryProps = ComponentProps<typeof Stack> & {
