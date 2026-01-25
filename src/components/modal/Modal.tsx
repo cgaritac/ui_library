24a49dc3e2305dd14033a70fdf8232cd6f9ac410
Carlos Garita Campos <cgaritac@gmail.com>
2026-01-25 00:56:19 -0600

    Feat: Add modal initial cnfiguration

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,7 +1,71 @@
-export const Modal = () => {
+import React, { useEffect, useRef, type PropsWithChildren } from 'react';
+import './modal.css';
+
+export type ModalProps = PropsWithChildren<{
+    open: boolean;
+    onClose: () => void;
+    title?: string;
+    'aria-label'?: string;
+    'aria-labelledby'?: string;
+    'aria-describedby'?: string;
+    role?: string;
+}>;
+
+export const Modal: React.FC<ModalProps> = ({ 
+    open,
+    onClose,
+    title = 'Modal',
+    children,
+    'aria-label': ariaLabel,
+    'aria-labelledby': ariaLabelledBy,
+    'aria-describedby': ariaDescribedBy,
+    role,
+}: ModalProps) => {
+    const dialogRef = useRef<HTMLDialogElement>(null);
+
+    useEffect(() => {
+        const dialog = dialogRef.current;
+        if (!dialog) return;
+
+        if (open) {
+            dialog.showModal();
+            dialog.focus();
+        } else {
+            dialog.close();
+        }
+    },[open]);
+
+    useEffect(() => {
+        const dialog = dialogRef.current;
+
+        const handleCancel = (e: Event) => {
+            e.preventDefault();
+            onClose();
+        };
+
+        dialog?.addEventListener('cancel', handleCancel);
+        return () => dialog?.removeEventListener('cancel', handleCancel);
+    }, [onClose]);
+
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDialogElement>) => {
+        if (e.target === e.currentTarget){
+            onClose();
+        }
+    };
+
     return (
-        <div>
-            <h1>Modal</h1>
-        </div>
+        <dialog
+            ref={dialogRef}
+            aria-label={ariaLabel}
+            aria-labelledby={ariaLabelledBy}
+            aria-describedby={ariaDescribedBy}
+            role={role}
+            onClick={handleBackdropClick}
+            className="cgc-modal"
+        >
+            <h2>{title}</h2>
+            {children}
+
+        </dialog>
     );
 };
--- src/stories/Modal.stories.tsx
+++ src/stories/Modal.stories.tsx
@@ -2,20 +2,74 @@ import type { ComponentProps } from "react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Modal } from "../components/modal/Modal";
 import './stories.css';
+import { fn } from "storybook/test";
 
 type StoryProps = ComponentProps<typeof Modal> & {
-    modalTitle: string;
-    modalContent: string;
-    modalFooter: string;
+    title: string;
+    open: boolean;
+    onClose: () => void;
+    'aria-label': string;
+    'aria-labelledby': string;
+    'aria-describedby': string;
+    role: string;
+    children: React.ReactNode;
 };
 
 const meta: Meta<StoryProps> = {
     title: 'Components/Modal',
     component: Modal,
     argTypes: {
-        modalTitle: {
+        title: {
             control: 'text',
             description: 'Title of the modal',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'Modal' },
+            },
+        },
+        open: {
+            control: 'boolean',
+            description: 'Whether the modal is open',
+        },
+        onClose: {
+            action: 'closed',
+            description: 'Callback function triggered when the modal is closed',
+            table: {
+                type: { summary: '() => void' },
+                defaultValue: { summary: 'undefined' },
+            },
+        },
+        'aria-label': {
+            control: 'text',
+            description: 'Accessibility label that describes the purpose of the modal',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'undefined' },
+            },
+        },
+        'aria-labelledby': {
+            control: 'text',
+            description: 'Accessibility label that describes the purpose of the modal',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'undefined' },
+            },
+        },
+        'aria-describedby': {
+            control: 'text',
+            description: 'Accessibility label that describes the purpose of the modal',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'undefined' },
+            },
+        },
+        role: {
+            control: 'text',
+            description: 'ARIA role to enhance accessibility',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'undefined' },
+            },
         },
     },
 };
@@ -26,8 +80,31 @@ type Story = StoryObj<StoryProps>;
 
 export const Default: Story = {
     args: {
-        modalTitle: 'Modal Title',
-        modalContent: 'Modal Content',
-        modalFooter: 'Modal Footer',
+        title: 'Modal Title',
+        open: true,
+        onClose: fn(),
+        'aria-label': 'Modal',
+        'aria-labelledby': 'Modal',
+        'aria-describedby': 'Modal',
+        role: 'dialog',
+        children: <div>
+            <h3>Modal Content</h3>
+            <p>This is the content of the modal.</p>
+            <button onClick={fn()}>Close</button>
+        </div>,
     },
+    parameters: {
+        docs: {
+            description: {
+                component: 'A modal component that allows the user to view and interact with a modal.',
+            },
+        },
+    },
+    decorators: [
+        (Story) => (
+            <div className="cgc--story-decorator">
+                <Story />
+            </div>
+        ),
+    ],
 };
